a607c3cd498ed36b2dfa3d80c95c6642e12ac3a3
FuryBrand <bindthemall@outlook.jp>
2020-01-03 22:09:33 +0800

    Update lesson01_08.py

--- python_crash_course/lesson01_08.py
+++ python_crash_course/lesson01_08.py
@@ -64,3 +64,11 @@ print(musician)
 musician = get_formatted_name('john', 'hooker', 'lee')
 print(musician)
 # 8.3.3 返回字典
+def build_person(first_name, last_name, age=''):
+    """返回一个字典，其中包含有关一个人的信息"""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)
