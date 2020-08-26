526bf54d01ff5279f8fee85c02d917023950dd13
FuryBrand <bindthemall@outlook.jp>
2020-08-26 04:31:26 +0000

    till 12.8.6

--- python_crash_course/lesson02_01_alien_invasion/alien_invasion.py
+++ python_crash_course/lesson02_01_alien_invasion/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -9,13 +10,22 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         # 更新飞船位置
         ship.update()
+        # 更新子弹位置
+        bullets.update()
+        # 删除已消失的子弹
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
         # 每次循环时都重新绘屏幕并让其可见
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 run_game()
