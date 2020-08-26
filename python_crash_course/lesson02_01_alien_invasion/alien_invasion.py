5d630e345e228d78972b8359dab1dcbce3b714e0
FuryBrand <bindthemall@outlook.jp>
2020-08-26 11:36:09 +0000

    lesson 02 done

--- python_crash_course/lesson02_01_alien_invasion/alien_invasion.py
+++ python_crash_course/lesson02_01_alien_invasion/alien_invasion.py
@@ -5,6 +5,7 @@ from ship import Ship
 import game_functions as gf
 from button import Button
 from game_stats import GameStats
+from scoreboard import Scoreboard
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
@@ -13,8 +14,9 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     # 创建Play按钮
     play_button = Button(ai_settings, screen, "Play")
-    # 创建一个用于存储游戏统计信息的实例
+    # 创建一个用于存储游戏统计信息的实例，同时创建计分板
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
     # 创建一艘飞船、一个子弹组、一个外星人组
     ship = Ship(ai_settings, screen)
     bullets = Group()
@@ -24,14 +26,14 @@ def run_game():
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
         if stats.game_active:
             # 更新飞船位置
             ship.update()
             # 更新子弹
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
             # 更新外星人
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
         # 每次循环时都重新绘屏幕并让其可见
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets, play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 run_game()
--- python_crash_course/lesson02_01_alien_invasion/settings.py
+++ python_crash_course/lesson02_01_alien_invasion/settings.py
@@ -2,22 +2,37 @@ class Settings():
     """存储《外星人入侵》的所有设置的类"""
     def __init__(self):
         """初始化游戏的设置"""
-        # 屏幕设置
+        # 设置屏幕大小和背景色RGB。默认是黑色，示例是白色
         self.screen_width = 1200
         self.screen_height = 800
-        # 设置背景色RGB。默认是黑色，下面是白色
         self.bg_color = (230, 230, 230)
         # 飞船的设置
-        self.ship_speed_factor = 1.5
         self.ship_limit = 3
         # 子弹设置
-        self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
         # 外星人设置
-        self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
-        # fleet_direction为1表示向右移，为-1表示向左移
+        # 加快游戏的速度
+        self.speedup_scale = 1.1
+        # 外星人点数的提高速度
+        self.score_scale = 1.5
+        self.initialize_dynamic_settings()
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+        # fleet_direction为1表示向右；为-1表示向左
         self.fleet_direction = 1
+        # 计分
+        self.alien_points = 50
+    def increase_speed(self):
+        """提高速度，和获取的点数"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
